20edaa46a96fd8a1002836693296f7ac6f6f02d4
Shrikant Patil <pailshrikant77@gmail.com>
2024-01-02 17:39:38 +0530

    removed unwanted imports

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from "react"
 import Loader from "react-loaders"
 import AnimatedLetters from "../AnimatedLetters"
 import "./index.scss"
-import { getDocs, collection } from "firebase/firestore"
-import { db } from "../../firebase"
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState("text-animate")
